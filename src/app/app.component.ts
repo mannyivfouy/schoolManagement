7171f3cc56481ba74ce2e53024e039d539688774
Fouy <mannyivfouy@gmail.com>
2025-09-19 19:28:37 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
